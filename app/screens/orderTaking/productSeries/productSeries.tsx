381e9367f668f7a840cc8394221020865b75df32
MantSingh <mantsingh@deloitte.com>
2024-10-08 21:27:31 +0530

    heading check icon , border gradient ,quatitiy UI updated

--- app/screens/orderTaking/productSeries/productSeries.tsx
+++ app/screens/orderTaking/productSeries/productSeries.tsx
@@ -120,9 +120,10 @@ const ProductSeries = () => {
         <FlatList
           data={getFilteredList()}
           showsVerticalScrollIndicator={false}
-          renderItem={({item}) => {
+          renderItem={({item, index}) => {
             return (
               <TouchableOpacity
+                activeOpacity={0.9}
                 onPress={() => {
                   sheetRef.current?.present();
                   setSelectedCardItem(item);
@@ -136,6 +137,7 @@ const ProductSeries = () => {
                   skuId={item.avl}
                   price={item.price}
                   itemQuantity={0}
+                  isBorderGradient={index % 2 == 0}
                 />
               </TouchableOpacity>
             );
