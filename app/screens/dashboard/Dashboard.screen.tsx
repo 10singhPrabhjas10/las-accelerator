df77f1ec7ff19572a884568edb6d98f3e69f02d9
Karthik Shetty <karshetty.ext@deloitte.com>
2024-11-08 15:30:16 +0530

    Created a ListContainer Component with styles for the Retailer Details Screen

--- app/screens/dashboard/Dashboard.screen.tsx
+++ app/screens/dashboard/Dashboard.screen.tsx
@@ -27,6 +27,8 @@ import {ScrollView} from 'react-native-gesture-handler';
 import QuickLinkCard from './components/QuickLinkCard';
 import CommonStyles from '@/utils/commonStyle';
 
+import ListComponent from '../beatPlan/components/listComponent/listComponent';
+
 interface IDashboardTileProps {
   title: string;
   icon: ReactNode;
@@ -158,6 +160,8 @@ const Dashboard = () => {
           )}
         />
       </View>
+
+      <ListComponent />
 
       <View style={[styles.component, styles.marginBottom50]}>
         <Text variant="headlineSmall" style={CommonStyles.marginVertical10}>
